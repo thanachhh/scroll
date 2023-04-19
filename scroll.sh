9448f9cbc2756386c91aecba9e72698ec9acbc59
thanachhh <90443443+thanachhh@users.noreply.github.com>
2023-04-19 16:41:20 +0700

    Add files via upload

---
+++ scroll.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt install yarn
+
+# Install nodejs and npm
+sudo apt update
+sudo apt install nodejs
+sudo apt install npm
+
+# Clone the repository and install dependencies
+git clone https://github.com/scroll-tech/scroll-contract-deploy-demo.git
+cd scroll-contract-deploy-demo
+yarn install
+
+# Upgrade dependencies
+yarn upgrade @nomicfoundation/hardhat-chai-matchers@1.0.4
+yarn add @ethersproject/bytes@^5.0.0
+yarn upgrade hardhat@latest 
+
+cd ..
+cd scroll-contract-deploy-demo
